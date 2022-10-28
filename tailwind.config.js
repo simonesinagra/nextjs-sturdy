811883828b7c29d258ee637efe3ad9ed19e58a42
Simone Sinagra <sis@bendingspoons.com>
2022-10-28 11:47:15 +0200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./(pages|components)/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
